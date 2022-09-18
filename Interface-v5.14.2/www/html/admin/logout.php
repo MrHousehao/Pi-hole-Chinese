d05595ed5218ce96133ff08d617c7974dd9bbe44
MrHousehao <91263873+MrHousehao@users.noreply.github.com>
2022-09-18 13:06:18 +0800

    -

--- Interface-v5.14.2/www/html/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// If the user wants to log out, we free all session variables currently registered
-// and delete any persistent cookie.
-session_start();
-session_unset();
-setcookie('persistentlogin', '', 1);
-header('Location: login.php');
-exit;
